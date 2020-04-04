7a28dca1198cb388cfc4cbdd9cbef90704773834
msa197 <39532801+msa197@users.noreply.github.com>
2020-04-04 16:20:48 -0700

    Update ArithUnit.vhd

--- ArithUnit.vhd
+++ ArithUnit.vhd
@@ -3,18 +3,17 @@ Use ieee.std_logic_1164.all;
 Use ieee.numeric_std.all;
 
 entity ArithUnit is
-		generic (width : integer := 64);
+		generic (N : integer := 64);
 		port (
-				A, B				:	in	std_logic_vector(width-1 downto 0);
+				A, B				:	in	std_logic_vector(N-1 downto 0);
 				NotA, AddSub, ExtWord		:	in	std_logic;
 				AltBu, AltB 			:	out 	std_logic;
-				Y				:	out	std_logic_vector(width-1 downto 0));
+				Y				:	out	std_logic_vector(N-1 downto 0));
 end entity ArithUnit;
 
 
 architecture rtl of ArithUnit is 
-signal Aout, Bout, S : std_logic_vector(width-1 downto 0);
-signal C : std_logic_vector(width downto 0);
+signal Aout, Bout, S : std_logic_vector(N-1 downto 0);
 signal Cin, Ovfl, Cout, Zero : std_logic;
 begin
 	Aout <= (A and not NotA) or (not A and NotA);
@@ -23,27 +22,14 @@ begin
 	x0: entity work.Adder 
 	port map(A, B, S, Cin, Cout, Ovfl);
 	
-	Zero <= S(width-1 downto 0) nor S(width-1 downto 0); -- not sure if this is correct, 64 bit input NOR
+	Zero <= S(N-1 downto 0) nor S(N-1 downto 0); -- not sure if this is correct, 64 bit input NOR
 	
 	Y <= (S and not ExtWord) or (not s and ExtWord);-- not sure what to do with the sign extension
 	
 	-- unsigned A less than B 
-	if (Zero==1) then 
-		AltBu=0;
-	elseif (Cout==0) then
-		AltBu=0;
-	else 
-		AltBu=1;
-	end if;
+	AltBu <= not Cout;
 	
 	-- signed A less than B
-	if (Zero==1) then 
-		AltB=0;
-	elseif (Ovfl==0) then
-		AltB=0;
-	else 
-		AltB=1;
-	end if;
-	
+	AltB <= S(N-1) xor Ovfl;
 	
 end architecture rtl;
