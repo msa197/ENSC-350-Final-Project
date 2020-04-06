13c6604b85013712a5a584a84d96766046904bcf
msa197 <39532801+msa197@users.noreply.github.com>
2020-04-05 17:17:13 -0700

    Update LogicUnit.vhd

--- LogicUnit.vhd
+++ LogicUnit.vhd
@@ -5,15 +5,15 @@ use ieee.numeric_std.all;
 entity LogicUnit is
 generic (N : natural := 64);
 port (A,B : in std_logic_vector(N-1 downto 0);
-	LogicFN : in std_logic_vector(1 downto 0);
+	LogicFN : in std_logic_vector(1 downto 0); 
 	Y : out std_logic_vector(N-1 downto 0));
 end entity LogicUnit;
 
 architecture rtl of LogicUnit is
 begin
-	with LogicFN select Y <=
+	with LogicFN select Y <=		--multiplexing A and B, using LogicFN to determine which logical output to output
 		(A xor B) when "01",
 		(A or B) when "10",
 		(A and B) when "11",
-		B when others;
+		B when others; 			-- Ouput is B when LogicFN is "00"
 end architecture rtl;
