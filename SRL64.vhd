732f4ccfaedbcb4d41a46b0897da448d4c9f7337
msa197 <39532801+msa197@users.noreply.github.com>
2020-04-08 15:19:17 -0700

    Update SRL64.vhd

--- SRL64.vhd
+++ SRL64.vhd
@@ -10,24 +10,24 @@ ShiftCount : in unsigned( integer(ceil(log2(real(N))))-1 downto 0 ) );
 End Entity SRL64;
 
 architecture rtl of SRL64 is
-begin
 signal a, b : std_logic_vector(N-1 downto 0);
+begin
 
 		
-		with ShiftCount (5 down to 4) select a <=
+		with ShiftCount (5 downto 4) select a <=
 		shift_right(unsigned(X),1) when "01",
 		shift_right(unsigned(X),2) when "10",
 		shift_right(unsigned(X),3) when "11",
 		X when others;
 		
 		
-		with ShiftCount (3 down to 2) select b <=
+		with ShiftCount (3 downto 2) select b <=
 		shift_right(unsigned(a),1) when "01",
 		shift_right(unsigned(a),2) when "10",
 		shift_right(unsigned(a),3) when "11",
 		a when others;
 		
-		with ShiftCount (1 down to 0) select Y <=
+		with ShiftCount (1 downto 0) select Y <=
 		shift_right(unsigned(b),1) when "01",
 		shift_right(unsigned(b),2) when "10",
 		shift_right(unsigned(b),3) when "11",
