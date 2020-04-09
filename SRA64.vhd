3ba6b742f912bcd64115dad8ba2cd6bfdb920550
msa197 <39532801+msa197@users.noreply.github.com>
2020-04-08 17:21:27 -0700

    Update SRA64.vhd

--- SRA64.vhd
+++ SRA64.vhd
@@ -1,6 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use IEEE.MATH_REAL.ALL;
+use IEEE.NUMERIC_STD.all;
 
 Entity SRA64 is -- Shift Right Arithmetic
 Generic ( N : natural := 64 );
@@ -10,27 +12,31 @@ ShiftCount : in unsigned( integer(ceil(log2(real(N))))-1 downto 0 ) );
 End Entity SRA64;
 
 architecture rtl of SRA64 is
-signal a, b : std_logic_vector(N-1 downto 0);
+signal a, b, c, d : std_logic_vector(N-1 downto 0);
+signal count : integer;
 begin
+	count <= to_integer(ShiftCount);
+		
+	with ShiftCount (5 downto 4) select a <=
+    	std_logic_vector(signed(X) srl 16) when "01",
+    	std_logic_vector(signed(X) srl 32) when "10",
+    	std_logic_vector(signed(X) srl 48) when "11",
+	X when others;
 
+	with ShiftCount (3 downto 2) select b <=
+    	std_logic_vector(signed(a) srl 4) when "01",
+    	std_logic_vector(signed(a) srl 8) when "10",
+    	std_logic_vector(signed(a) srl 12) when "11",
+	a when others;
+
+	with ShiftCount (1 downto 0) select c <=
+    	std_logic_vector(signed(b) srl 1) when "01",
+    	std_logic_vector(signed(b) srl 2) when "10",
+    	std_logic_vector(signed(b) srl 3) when "11",
+	b when others;
 		
-		with ShiftCount (5 downto 4) select a <=
-		shift_right(signed(X),1) when "01",
-		shift_right(signed(X),1) when "10",
-		shift_right(signed(X),1) when "11",
-		X when others;
-		
-		
-		with ShiftCount (3 downto 2) select b <=
-		shift_right(signed(a),1) when "01",
-		shift_right(signed(a),2) when "10",
-		shift_right(signed(a),3) when "11",
-		a when others;
-		
-		with ShiftCount (1 downto 0) select Y <=
-		shift_right(signed(b),1) when "01",
-		shift_right(signed(b),2) when "10",
-		shift_right(signed(b),3) when "11",
-		b when others;
+	d <= c(N-count downto 0);
+	Y(N-1 downto (N-count+1)) <= (others => c(N-count));
+	Y(N-count downto 0) <= d;
 
 end architecture rtl;
